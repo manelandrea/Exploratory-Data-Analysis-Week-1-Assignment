fd9c5f8cd49fedfe986625c8d9d32daf72d6074c
manelandrea <nel_cementina@yahoo.com>
2018-03-09 16:58:32 +0800

    plot 3

---
+++ plot3.R
@@ -0,0 +1,34 @@
+active_power<-read.table("household_power_consumption.txt",header = TRUE, sep= ";")
+
+
+active_power$DateTime<-paste(active_power$Date, data$Time)
+
+#Modify Date and Time to appropriate format
+
+active_power$DateTime<-strptime(active_power$DateTime, "%d/%m/%Y %H:%M:%S")
+
+# extract 2007-02-01 and 2007-02-02
+
+fromDate <-which(active_power$DateTime==strptime("2007-02-01", "%Y-%m-%d"))
+
+toDate <-which(active_power$DateTime==strptime("2007-02-02 23:59:00", "%Y-%m-%d %H:%M:%S"))
+
+global_active_power<-active_power[start:end,]
+
+#plot data
+
+png(file="plot4.png", width=480, height=480)
+
+plot(global_active_power$DateTime,as.numeric(as.character(global_active_power$Sub_metering_1)), type="l",
+     main="Energy sub metering",
+     ylab="Energy sub metering", xlab="")
+
+lines(global_active_power$DateTime,as.numeric(as.character(global_active_power$Sub_metering_2)), type="l",
+     main="Energy sub metering",
+     ylab="Energy sub metering", xlab="",col="red")
+lines(global_active_power$DateTime,as.numeric(as.character(global_active_power$Sub_metering_3)), type="l",
+      main="Energy sub metering",
+      ylab="Energy sub metering", xlab="",col="blue")
+
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty = c(1,1,1),col=c("black","red","blue"))
+dev.off()
